daed0bbb13911d7ae1d5e5c23741aff2ad1ead87
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2022-11-11 12:50:00 +0530

    adding code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import Header from "./components/header/Header";
+import SidebarLeft from "./components/sidebar/SidebarLeft";
+import SidebarRight from "./components/sidebar/SidebarRight";
+import { auth } from "./config/firebase";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(false);
+      }
+    });
+  });
+
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Header user={user} selected />
+                <div className="app__page">
+                  <SidebarLeft user={user} />
+                  <SidebarRight user={user} />
+                </div>
+              </div>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./index.css";
 import App from "./App";
 import ReactDOM from "react-dom/client";
+import { StateProvider } from "./store/StateProvider";
+import reducer, { initialState } from "./reducers/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
